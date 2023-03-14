340a257753451ebea3a3f89cf5b05caee7b3a303
VoTienTai30 <midnight30122000@gmail.com>
2023-03-15 02:46:27 +0700

    add currency system

--- Assets/Scripts/CurrencySystem.cs
+++ Assets/Scripts/CurrencySystem.cs
@@ -1,31 +1,24 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CurrencySystem : MonoBehaviour
 {
-    //FIELDS
-    //currency txt UI
-    public Text txt_Currency;
-    //default currency value
+    public TextMeshProUGUI txt_Currency;
     public int defaultCurrency;
-    //current currency value
     public int currency;
 
 
-    //METHODS
-    //Init (set the default values)
     public void Init()
     {
         currency = defaultCurrency;
         UpdateUI();
     }
-    //Gain currency (input of value)
     public void Gain(int val)
     {
         currency += val;
         UpdateUI();
     }
-    //Use currency (input of value)
     public bool Use(int val)
     {
         if (EnoughCurrency(val))
@@ -39,16 +32,13 @@ public class CurrencySystem : MonoBehaviour
             return false;
         }
     }
-    //Check availability of currency
     public bool EnoughCurrency(int val)
     {
-        //Check if the val is equal or more than currency
         if (val <= currency)
             return true;
         else
             return false;
     }
-    //Update txt ui
     void UpdateUI()
     {
         txt_Currency.text = currency.ToString();
