102c8f611acb794f6421645316852585633cb2a4
VoTienTai30 <midnight30122000@gmail.com>
2023-03-15 02:46:17 +0700

    add currency system UI

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,17 +6,11 @@ using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
-    //list of towers (prefabs) that will instantiate
     public List<GameObject> towersPrefabs;
-    //Transform of the spawning towers (Root Object)
     public Transform spawnTowerRoot;
-    //list of towers (UI)
     public List<Image> towersUI;
-    //id of tower to spawn
     int spawnID=-1;
-    //SpawnPoints Tilemap
     public Tilemap spawnTilemap;
-    //Tile position
     private Vector3Int tilePos;
     
     void Update()
@@ -35,16 +29,11 @@ public class Spawner : MonoBehaviour
 
     void DetectSpawnPoint()
     {
-        //Detect when mouse is clicked (first touch clicked)
         if(Input.GetMouseButtonDown(0))
         {
-            //get the world space postion of the mouse
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //get the position of the cell in the tilemap
             var cellPosDefault = spawnTilemap.WorldToCell(mousePos);
-            //get the center position of the cell
             var cellPosCentered = spawnTilemap.GetCellCenterWorld(cellPosDefault);
-            //check if we can spawn in that cell (collider)
             if(spawnTilemap.GetColliderType(cellPosDefault)== Tile.ColliderType.Sprite)
             {
                // int towerCost = TowerCost(spawnID);
