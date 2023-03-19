e5f00e25350cf6123f2a0a414406a718a7962a8e
DatVT <datvthe151334@fpt.edu.vn>
2023-03-20 00:23:50 +0700

    update load game

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -36,8 +36,8 @@ public class Menu : MonoBehaviour
         float enemySpawnRateIncrease = 0;
         int numberEnemySpawn = 2;
         float distanceBetweenObjects = 3;
-        string[] hero;
-        string[] enermy;
+        //string[] hero;
+        //string[] enermy;
         string filePath = Application.dataPath + "/savegame.txt";
         if (System.IO.File.Exists(filePath))
         {
@@ -50,87 +50,112 @@ public class Menu : MonoBehaviour
                 {
                     string[] parts = line.Split(':');
                      health = int.Parse(parts[1]);
-                    
+                   var healthsys = new HealthSystem();
+                    healthsys.defaultHealthCount = health;
                 }
                 if (i == 1)
                 {
                     string[] parts = line.Split(':');
                     money = int.Parse(parts[1]);
+                    var currency =new CurrencySystem();
+                    currency.currency = money;
                 }
                 if (i == 2)
                 {
-                    string pattern = @"\((.*?)\)";
+                    string pattern = "\\(Tower[123]\\(Clone\\) [-+]?[0-9]*\\.?[0-9]+ [-+]?[0-9]*\\.?[0-9]+ [-+]?[0-9]*\\.?[0-9]+\\)";
 
                     // Find all matches of the pattern in the input string
                     MatchCollection matches = Regex.Matches(line, pattern);
 
-                    // Create a new array to store the tower positions
-                    string[] towerPositions = new string[matches.Count];
+                    // Create a new list to store the tower positions
+                    List<string> towerPositions = new List<string>();
 
                     // Loop through each match and extract the tower position
-                    for (int j  = 0; j  < matches.Count; j ++)
+                    foreach (Match match in matches)
                     {
+
                         // Extract the tower position from the match
-                        string towerPosition = matches[j].Groups[0].Value;
-                        Debug.Log(towerPosition);
-                        // Add the result string to the tower positions array
-                        towerPositions[j] = towerPosition;
+                        string towerPosition = match.Groups[0].Value;
+
+                        // Add the result string to the tower positions list
+                        towerPositions.Add(towerPosition);
                     }
 
                     // Output the tower positions
-                    //foreach (string position in towerPositions)
-                    //{
-                    //    Debug.Log(position);
-                    //}
+                    foreach (string position in towerPositions)
+                    {
+                        Debug.Log(position);
+                    }
                 }
                 if (i == 3)
                 {
-                    string pattern = @"\((.*?)\)";
+                    string pattern = "\\(Enemy[123]\\(Clone\\) [-+]?[0-9]*\\.?[0-9]+ [-+]?[0-9]*\\.?[0-9]+ [-+]?[0-9]*\\.?[0-9]+\\)";
+
+                    // Find all matches of the pattern in the input string
                     MatchCollection matches = Regex.Matches(line, pattern);
-                    enermy = new string[matches.Count];
-                    for (int j = 0; j < matches.Count; j++)
+
+                    // Create a new list to store the tower positions
+                    List<string> towerPositions = new List<string>();
+
+                    // Loop through each match and extract the tower position
+                    foreach (Match match in matches)
                     {
 
-                        string positionString = matches[j].Groups[1].Value;
-                        
 
-                        string resultString = positionString;
+                        // Extract the tower position from the match
+                        string towerPosition = match.Groups[0].Value;
 
-                        // Add the result string to the tower positions array
-                        enermy[j] = resultString;
+                        // Add the result string to the tower positions list
+                        towerPositions.Add(towerPosition);
                     }
-                 
-                    
+
+                    // Output the tower positions
+                    foreach (string position in towerPositions)
+                    {
+                        Debug.Log(position);
+                    }
                 }
                 if (i == 4)
                 {
                     string[] parts = line.Split(':');
                     spawnerTime = int.Parse(parts[1]);
+                    
                 }
                 if (i == 5)
                 {
                     string[] parts = line.Split(':');
                     percentRandomBoss = int.Parse(parts[1]);
+                    var spanwer = new EnemySpawner();
+                    spanwer.percentRandomBoss = percentRandomBoss;
                 }
                 if (i == 6)
                 {
                     string[] parts = line.Split(':');
                     countTimeSpawn = int.Parse(parts[1]);
+                    var spanwer = new EnemySpawner();
+                    spanwer.countTimeSpawn = countTimeSpawn;
                 }
                 if (i == 7)
                 {
                     string[] parts = line.Split(':');
                     enemySpawnRateIncrease = float.Parse(parts[1]);
+                    var spanwer = new EnemySpawner();
+                    spanwer.enemySpawnRateIncrease = enemySpawnRateIncrease;
+
                 }
                 if (i == 8)
                 {
                     string[] parts = line.Split(':');
                     numberEnemySpawn = int.Parse(parts[1]);
+                    var spanwer = new EnemySpawner();
+                    spanwer.numberEnemySpawn = numberEnemySpawn;
                 }
                 if (i == 9)
                 {
                     string[] parts = line.Split(':');
                     distanceBetweenObjects = float.Parse(parts[1]);
+                    var spanwer = new EnemySpawner();
+                    spanwer.distanceBetweenObjects = distanceBetweenObjects;
                 }
 
                 i++;
